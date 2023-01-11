fe77d187e8ead86e9a70b309bb38219ac75a284f
akshita <akshita18keshri@gmail.com>
2023-01-11 16:11:10 +0530

    first commit

---
+++ A6CollegeService/src/main/java/com/springboot/CollegeController.java
@@ -0,0 +1,65 @@
+package com.springboot;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+//eliminate the need to annotate every request handling method
+
+public class CollegeController {
+	@Autowired//dependency injection
+	private CollegeService CollegeService;
+	
+	//Retrieve
+	@GetMapping("/Colleges")
+	public List<College> list(){
+		return CollegeService.listAll();
+	}
+	
+	//Retrieve By Id
+	@GetMapping("/Colleges/{id}")
+	public ResponseEntity get(@PathVariable Integer id) {
+		try {
+			College College=CollegeService.get(id);
+			return new ResponseEntity<College>(College,HttpStatus.OK);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			return new ResponseEntity<College>(HttpStatus.NOT_FOUND);
+		}
+	}
+	
+	//create 
+	@PostMapping("/Colleges")
+	public void add(@RequestBody College College) {//annotation maps the HttpRequest body to a transfer or domain object
+		CollegeService.save(College);
+	}
+	
+	//update 
+	@PutMapping("/Colleges/{id}")
+	public ResponseEntity<?> update(@RequestBody College College,@PathVariable Integer id){
+		try {
+			College updateCollege=CollegeService.get(id);
+			CollegeService.save(College);
+			return new ResponseEntity<>(HttpStatus.OK);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+	}
+	
+	//delete
+	@DeleteMapping("/Colleges/{id}")
+	public void delete(@PathVariable Integer id) {
+		CollegeService.delete(id);
+	}
+}
---
+++ A6CollegeService/src/main/java/com/springboot/CollegeRepository.java
@@ -0,0 +1,10 @@
+package com.springboot;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+
+public interface CollegeRepository extends JpaRepository<College,Integer>{
+
+}
